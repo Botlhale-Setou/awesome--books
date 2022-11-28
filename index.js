b4041a132ec04ecc48cc0285db1b6b1f7292faf3
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-11-28 20:27:33 +0530

    Fixed linters

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
-let books_list = [];
+let booksList = [];
 
-const add_to_books_list = (title, author) => {
-  let book = {
+const addToBooksList = (title, author) => {
+  const book = {
     tit: title,
-    aut: author
-  }
-  books_list.push(book);
-}
-
-const display_books = () => {
-  const book_section = document.querySelector('.books-list');
-  book_section.innerHTML = "";
-
-  for (let i=0; i<books_list.length; i+=1) {
-    book_section.innerHTML += `
-      <h4>${books_list[i].tit}</h4>
-      <h4>${books_list[i].aut}</h4>
-      <button id="${i}" class="rmv" type='button'>Remove</button>
-      <hr>
-    `
-  }
-}
-
-const add_button = document.querySelector('#button');
-add_button.addEventListener("click", () => {
-  const title_input = document.querySelector('#title').value;
-  const author_input = document.querySelector('#author').value;
-  add_to_books_list(title_input, author_input);
-  display_books();
-  setRmvs();
-
-  window.localStorage.setItem('books', JSON.stringify(books_list));
-});
-
-
-const setRmvs = () => {
-  let xallRmvs = document.getElementsByClassName('rmv');
-  let allRmvs = Array.from(xallRmvs);
-
-  allRmvs.forEach(btn => {
-    btn.addEventListener('click', () => {
-      remove_book(btn.id);
-      display_books();
-      setRmvs();
-    })
-  });
-  
+    aut: author,
+  };
+  booksList.push(book);
 };
 
-const remove_book = (ix) => {
-  books_list.splice(ix, 1);
-}
+const displayBooks = () => {
+  const bookSection = document.querySelector('.books-list');
+  bookSection.innerHTML = '';
+
+  for (let i = 0; i < booksList.length; i += 1) {
+    bookSection.innerHTML += `
+      <h4>${booksList[i].tit}</h4>
+      <h4>${booksList[i].aut}</h4>
+      <button id="${i}" class="rmv" type='button'>Remove</button>
+      <hr>
+    `;
+  }
+};
+
+const removeBook = (ix) => {
+  booksList.splice(ix, 1);
+};
+
+const setRmvs = () => {
+  const xallRmvs = document.getElementsByClassName('rmv');
+  const allRmvs = Array.from(xallRmvs);
+
+  allRmvs.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      removeBook(btn.id);
+      displayBooks();
+      setRmvs();
+      window.localStorage.setItem('books', JSON.stringify(booksList));
+    });
+  });
+};
+
+const addButton = document.querySelector('#button');
+addButton.addEventListener('click', () => {
+  const titleInput = document.querySelector('#title').value;
+  const authorInput = document.querySelector('#author').value;
+  addToBooksList(titleInput, authorInput);
+  displayBooks();
+  setRmvs();
+
+  window.localStorage.setItem('books', JSON.stringify(booksList));
+});
 
 window.onload = () => {
   const localStorageItem = window.localStorage.getItem('books');
 
   if (localStorageItem) {
-    books_list = JSON.parse(localStorageItem);
+    booksList = JSON.parse(localStorageItem);
 
-    display_books()
+    displayBooks();
     setRmvs();
   }
 };

a67742fa5e6ce2d4aaf73d53f944e186398c278c
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-11-30 18:26:20 +0530

    Added js to add new, list in the navbar

--- index1.js
+++ index1.js
@@ -15,7 +15,7 @@ class AwesomeBooks {
 
   displayBooks = () => {
     const bookSection = document.querySelector('.books-list');
-    bookSection.innerHTML = '';
+    bookSection.innerHTML = ``;
 
     for (let i = 0; i < this.booksList.length; i += 1) {
       bookSection.innerHTML += `
@@ -65,3 +65,22 @@ window.onload = () => {
     setRmvs();
   }
 };
+
+const listNav = document.getElementById('list__nav1');
+const addNav = document.getElementById('list__nav2');
+const contNav = document.getElementById('list__nav3');
+const inputs = document.querySelector('.inputs');
+const bookSec = document.querySelector('.list_div');
+
+
+listNav.addEventListener('click', () => {
+  inputs.classList.add('hide');
+  inputs.classList.remove('show');
+  bookSec.classList.add('show');
+})
+
+addNav.addEventListener('click', () => {
+  bookSec.classList.remove('show');
+  bookSec.classList.add('hide');
+  inputs.classList.add('show2');
+})
